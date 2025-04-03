e7886cdf4c861c4d8058055678f392a9c7c1028d
5QINYU5 <zhxwy.979@outlook.com>
2025-04-03 22:22:32 +0800

    Update db.js

--- api/db/db.js
+++ api/db/db.js
@@ -1,8 +1,8 @@
 const mysql=require('mysql')
 const db=mysql.createPool({
 	host:'127.0.0.1',
-	user:'root',
-	password:'121212',
+	user:'数据库账号',
+	password:'数据库密码',
 	database:'api'
 })
 module.exports=db
